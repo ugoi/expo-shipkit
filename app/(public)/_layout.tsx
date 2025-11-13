3709b8e2d978455ab61c947b86d9540e27eeb8dd
Stefan Dukic <96357300+ugoi@users.noreply.github.com>
2025-11-13 13:40:33 +0100

    fix android ui

--- app/(public)/_layout.tsx
+++ app/(public)/_layout.tsx
@@ -1,29 +1,29 @@
 import { Stack } from "expo-router";
 
 import { useThemeColor } from "@/hooks/useThemeColor";
-
+import { Platform } from "react-native";
 
 export default function PublicLayout() {
   const backgroundColor = useThemeColor({}, "background");
   const textColor = useThemeColor({}, "text");
   const tintColor = useThemeColor({}, "tint");
   const iconColor = useThemeColor({}, "icon");
-
   return (
     <Stack initialRouteName="welcome">
       <Stack.Screen
         name="welcome"
         options={{
           title: "Welcome",
-          headerTransparent: true,
+          headerTransparent: Platform.OS === "ios",
         }}
       />
       <Stack.Screen
         name="sign-in"
         options={{
           title: "Sign In",
-          headerTransparent: true,
+          headerTransparent: Platform.OS === "ios",
           headerLargeTitle: true,
+          headerShadowVisible: false,
           headerBackButtonDisplayMode: "minimal",
           headerTintColor: tintColor,
         }}
@@ -32,8 +32,9 @@ export default function PublicLayout() {
         name="email-confirmation"
         options={{
           title: "Email Confirmation",
-          headerTransparent: true,
+          headerTransparent: Platform.OS === "ios",
           headerLargeTitle: true,
+          headerShadowVisible: false,
           headerBackButtonDisplayMode: "minimal",
           headerTintColor: tintColor,
         }}
